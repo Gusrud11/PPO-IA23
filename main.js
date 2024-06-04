7786a6f3f5d6eab9c1d96b4ecc23c99e5f10ad39
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2024-06-04 18:01:16 +0000

    botao pesquisa dando erro ao dar o click

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 void async function () {
-    const searchBar = document.querySelector(".search-bar")
-    const inputSearch = searchBar.querySelector("input")
-    const searchResults = document.querySelector(".search-results")
-    const response = await fetch('dados.json')
-    const items = await response.json()
+    const searchBar = document.querySelector(".search-bar");
+    const inputSearch = searchBar.querySelector("input");
+    const searchResults = document.querySelector(".search-results");
+    const searchButton = searchBar.querySelector(".search-button");
+    const response = await fetch('dados.json');
+    const items = await response.json();
 
     const searchFn = value => {
-        const pessoasFiltradas = items.filter(pessoa => pessoa.nome.toLowerCase().includes(value.toLowerCase()))
-        searchResults.innerHTML = ""
+        const pessoasFiltradas = items.filter(pessoa => 
+            pessoa.nome.toLowerCase().includes(value.toLowerCase())
+        );
+        searchResults.innerHTML = "";
         pessoasFiltradas.forEach(pessoa => {
             searchResults.innerHTML += `
                 <li>
-                    ${pessoa.nome} - 
-                    ${pessoa.email}
-                    
+                  <button id="pessoa">  ${pessoa.nome} - 
+                    ${pessoa.email} </button>
                 </li>
-            `
-        })
-    }
-    
+            `;
+        });
+    };
+
     inputSearch.addEventListener("keydown", ev => {
-        if (ev.key == "Enter") {
-            searchFn(inputSearch.value)
+        if (ev.key === "Enter") {
+            searchFn(inputSearch.value);
         }
-    })
+    });
+
+    searchButton.addEventListener("click", () => {
+        searchFn(inputSearch.value);
+    });
 }();
